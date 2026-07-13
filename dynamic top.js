21ce8e5119ece43badd7b6c682487568069cd8de
WingedMolotov <136846943+WingedMolotov@users.noreply.github.com>
2026-07-13 18:16:17 -0500

    overflow: hidden to body when nav menu is active

--- dynamic top.js
+++ dynamic top.js
@@ -1,10 +1,13 @@
 const burger = document.getElementById('burger');
 const toprow = document.querySelector('#toprow ul');
 const close = document.getElementById('close');
+const body = document.body;
 
 burger.addEventListener('click', () => {
     toprow.classList.add('open');
+    body.style.overflow = 'hidden';
 });
 close.addEventListener('click', () => {
     toprow.classList.remove('open');
+    body.style.overflow = 'auto';
 });
