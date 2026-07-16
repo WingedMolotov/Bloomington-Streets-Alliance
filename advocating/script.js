67c4ef8273f17ce44ce035db3ef8040e97a58476
WingedMolotov <136846943+WingedMolotov@users.noreply.github.com>
2026-07-15 22:00:31 -0500

    advocating and resources page mobile friendly

---
+++ advocating/script.js
@@ -0,0 +1,4 @@
+toggleDetails(document.getElementById('ltbtoggle'), document.getElementById('ltbwrap'), false);
+toggleDetails(document.getElementById('cctoggle'), document.getElementById('ccwrap'), false);
+toggleDetails(document.getElementById('sctoggle'), document.getElementById('scwrap'), false);
+toggleDetails(document.getElementById('lctoggle'), document.getElementById('lcwrap'), false);
